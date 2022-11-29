0df55145b85d21920d56c8d94e4c77b99a78a065
ahmad <eng.ahmed.anwar@gmail.com>
2022-11-29 14:36:50 +1300

    delete all code related to storing a user in redux

--- client/api.js
+++ client/api.js
@@ -27,30 +27,6 @@ export function updateFruit(fruit, token) {
     .catch(logError)
 }
 
-export function deleteFruit(id, token) {
-  return request
-    .delete(`${rootUrl}/fruits/${id}`)
-    .set('authorization', `Bearer ${token}`)
-    .then((res) => res.body.fruits)
-    .catch(logError)
-}
-
-export function getUser(token) {
-  return request
-    .get(`${rootUrl}/users`)
-    .set('Authorization', `Bearer ${token}`)
-    .then((res) => res.body)
-    .catch(logError)
-}
-
-export function addUser(user, token) {
-  return request
-    .post(`${rootUrl}/users`)
-    .set('Authorization', `Bearer ${token}`)
-    .send(user)
-    .catch(logError)
-}
-
 function logError(err) {
   if (err.response.text === 'Username Taken') {
     throw new Error('Username already taken - please choose another')
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,43 +1,15 @@
-import React, { useEffect } from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import { useCacheUser } from '../auth0-utils'
-import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 import Nav from './Nav'
 import Fruits from './Fruits'
-import { getUser } from '../api'
-import Register from './Register'
-import { useDispatch } from 'react-redux'
-import { clearLoggedInUser, updateLoggedInUser } from '../slices/user'
 
 function App() {
-  useCacheUser()
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0()
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      dispatch(clearLoggedInUser())
-    } else {
-      getAccessTokenSilently()
-        .then((token) => getUser(token))
-        .then((userInDb) => {
-          userInDb
-            ? dispatch(updateLoggedInUser(userInDb))
-            : navigate('/register')
-        })
-        .catch((err) => console.error(err))
-    }
-  }, [isAuthenticated])
-
   return (
     <>
       <Nav />
       <Routes>
         <Route path="/" element={<Fruits />} />
-        <Route path="register" element={<Register />} />
       </Routes>
     </>
   )
