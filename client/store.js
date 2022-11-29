3b91db8e0e860eb7c9bed9c0bf024c8d6a0a1e0c
ahmad <eng.ahmed.anwar@gmail.com>
2022-11-29 15:14:43 +1300

    add fruits reducer to store

--- client/store.js
+++ client/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import user from './slices/user'
+
+import fruits from './slices/index'
 
 const store = configureStore({
   reducer: {
-    user,
+    fruits,
   },
 })
 
