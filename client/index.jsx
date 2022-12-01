afc80fef0f66c2d4872b16d39ef1c5a59a82e588
ciara <cbdoelman@gmail.com>
2022-12-01 16:39:32 +1300

    remove errors in index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -1,26 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Auth0Provider } from '@auth0/auth0-react'
+
 import App from './components/App'
 import { Provider } from 'react-redux'
+import { createRoot } from 'react-dom/client';
 
 import store from './slices'
 
+const container = document.getElementById('app');
+const root = createRoot(container);
+
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Auth0Provider
-      domain="dev-l15ujwk4.au.auth0.com"
-      clientId="W1zHcnGihSz4yraMmDFQ8NNrOQsOjFEW"
-      redirectUri={window.location.origin}
-      audience="https://fruits/api"
-    >
-      <Provider store={store}>
-        <Router>
-          <App />
-        </Router>
-      </Provider>
-    </Auth0Provider>,
-    document.getElementById('app')
+  root.render(
+
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   )
 })
