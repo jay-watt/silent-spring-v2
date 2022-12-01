bb08aedebe8c77c110adb46f3d0ed765545a3c75
ciara <cbdoelman@gmail.com>
2022-12-01 16:59:52 +1300

    start playing with styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,12 +3,11 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 
 import Box from './Box'
-import Controls from './Controls'
 
 function App() {
   return (
     <>
-      <main className="flex items-center justify-center h-screen">
+      <main className="flex items-center justify-center h-screen bg-violet-300	">
         <Canvas>
           <ambientLight intensity={0.5} />
           <spotLight position={[10, 10, 10]} angle={0.95} penumbra={1} />
@@ -16,6 +15,7 @@ function App() {
           <Box position={[-1.2, 0, 0]} />
           <Box position={[1.2, 0, 0]} />
           <Box position={[3.6, 2.4, 0]} />
+          <Box position={[5.9, 6.0, 1.0]} />
           <OrbitControls />
         </Canvas>
       </main>
--- client/components/Box.jsx
+++ client/components/Box.jsx
@@ -8,17 +8,17 @@ function Box(props) {
   // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  // // Subscribe this component to the render-loop, rotate the mesh every frame
+  // useFrame((state, delta) => (ref.current.rotation.x += delta))
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
       {...props}
       ref={ref}
       scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
