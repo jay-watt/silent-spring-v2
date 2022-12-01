5c773cf0b524171e652e838be65488f185a28273
jay <jay.watt8000@gmail.com>
2022-12-01 17:25:52 +1300

    render two components with different mp3 files

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,77 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-
-import Nav from './Nav'
-import Fruits from './Fruits'
-import Profile from './Profile'
+/* eslint-disable react/no-unknown-property */
+import * as THREE from 'three'
+import React, { Suspense, useRef, useEffect, useState } from 'react'
+import {
+  Canvas,
+  extend,
+  useThree,
+  useFrame,
+  useLoader,
+} from 'react-three-fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 function App() {
+  extend({ OrbitControls })
+
+  const Controls = () => {
+    const { camera, gl } = useThree()
+    const ref = useRef()
+    useFrame(() => ref.current.update())
+    return (
+      <orbitControls
+        ref={ref}
+        target={[0, 0, 0]}
+        enableDamping
+        args={[camera, gl.domElement]}
+      />
+    )
+  }
+
+  function Sound({ url }) {
+    const sound = useRef()
+    const { camera } = useThree()
+    const [listener] = useState(() => new THREE.AudioListener())
+    const buffer = useLoader(THREE.AudioLoader, url)
+
+    useEffect(() => {
+      sound.current.setBuffer(buffer)
+      sound.current.setRefDistance(1)
+      sound.current.setLoop(true)
+      sound.current.play()
+      camera.add(listener)
+      return () => camera.remove(listener)
+    }, [])
+    return <positionalAudio ref={sound} args={[listener]} />
+  }
+
+  function handleClick(event) {
+    event.preventDefault()
+    const audioCtx = new AudioContext()
+    audioCtx.resume()
+  }
+
   return (
     <>
-      <Nav />
-      <main className="flex items-center justify-center h-screen">
-        <Routes>
-          <Route path="/" element={<Fruits />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </main>
+      <button onClick={handleClick}>PRESS ME</button>
+      <Canvas camera={{ position: [0, 0, 10] }}>
+        <Suspense fallback={null}>
+          <mesh position={[20, 0, 0]}>
+            <boxBufferGeometry attach="geometry" />
+            <meshBasicMaterial attach="material" color="blue" />
+            <Sound url="./server/public/audio/1.mp3" />
+          </mesh>
+        </Suspense>
+
+        <Suspense fallback={null}>
+          <mesh position={[-20, 0, 0]}>
+            <boxBufferGeometry attach="geometry" />
+            <meshBasicMaterial attach="material" color="hotpink" />
+            <Sound url="./server/public/audio/2.mp3" />
+          </mesh>
+        </Suspense>
+
+        <Controls />
+      </Canvas>
     </>
   )
 }
--- client/index.jsx
+++ client/index.jsx
@@ -1,26 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { Auth0Provider } from '@auth0/auth0-react'
 import App from './components/App'
-import { Provider } from 'react-redux'
-
-import store from './slices'
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Auth0Provider
-      domain="dev-l15ujwk4.au.auth0.com"
-      clientId="W1zHcnGihSz4yraMmDFQ8NNrOQsOjFEW"
-      redirectUri={window.location.origin}
-      audience="https://fruits/api"
-    >
-      <Provider store={store}>
-        <Router>
-          <App />
-        </Router>
-      </Provider>
-    </Auth0Provider>,
-    document.getElementById('app')
-  )
+  ReactDOM.render(<App />, document.getElementById('app'))
 })
