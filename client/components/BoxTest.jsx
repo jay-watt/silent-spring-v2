bb1a3c360d12f7cbf07a230f8f4b20e7b632305e
anna <annaqiu527@gmail.com>
2022-12-02 15:52:39 +1300

    version with bug

--- client/components/BoxTest.jsx
+++ client/components/BoxTest.jsx
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from 'react'
-// import { useFrame } from '@react-three/fiber'
 
+import React, { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+import Sound from './Sound'
 
 function BoxTest(props) {
   // This reference gives us direct access to the THREE.Mesh object
@@ -11,15 +12,22 @@ function BoxTest(props) {
   // // Subscribe this component to the render-loop, rotate the mesh every frame
   // useFrame((state, delta) => (ref.current.rotation.x += delta))
   // Return the view, these are regular Threejs elements expressed in JSX
+
+
+  const audioUrl = `./server/public/audio/${props.data.ML_Catalog_Number}.mp3`
+  console.log(props.data.ML_Catalog_Number)
+
   return (
     <mesh
       ref={ref}
       scale={clicked ? 1.5 : 1}
       onClick={() => click(!clicked)}
       onPointerOver={() => hover(true)}
-      onPointerOut={() => hover(false)}>
+      onPointerOut={() => hover(false)}
+    >
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <Sound url={audioUrl} />
     </mesh>
   )
 }
--- client/components/List.jsx
+++ client/components/List.jsx
@@ -15,10 +15,19 @@ function List() {
       setData(() => birds)
     }).catch((err => err.message))
   }, [])
-  console.log(data)
+
   // useEffect(() => {
   //   fetchBirds()
   // }, [])
+  function getRandomInt() {
+    return Math.floor(Math.random() * 15 + 1)
+  }
+
+
+  const x = getRandomInt()
+  const y = getRandomInt()
+  const z = getRandomInt()
+
 
   return (<>
     {data.length > 0 ? (<main className="flex items-center justify-center h-screen bg-violet-300	">
@@ -27,7 +36,7 @@ function List() {
         <spotLight position={[10, 10, 10]} angle={0.95} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
         {data.map((data) => {
-          return <BoxTest position={[-1.2, 0, 0]} data={data} />
+          return <BoxTest key={data.id} position={[{ x }, { y }, { z }]} data={data} />
         })}
         {/*         
         <Box position={[1.2, 0, 0]} />
