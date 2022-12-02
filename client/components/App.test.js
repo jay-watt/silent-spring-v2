dde8621ab6bd4301175d57a2c95550ad00862cc9
ciara <cbdoelman@gmail.com>
2022-12-03 10:57:06 +1300

    push for merge

--- client/components/App.test.js
+++ client/components/App.test.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { screen, render } from '@testing-library/react'
 import App from './App'
 
+// this test does not work
+
 test('App div should contain handleStart function', async () => {
   render(<App />)
 
