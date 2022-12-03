bff995cc5ea946d6f7bc266fa1e19c481bfe37db
ciara <cbdoelman@gmail.com>
2022-12-03 13:37:06 +1300

    add escape key event listener to go back to wait page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { extend } from '@react-three/fiber'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Wait from './Wait'
@@ -10,6 +10,19 @@ function App() {
   function handleStart() {
     setIsPlaying(true)
   }
+
+  useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.keyCode === 27) {
+        setIsPlaying(false)
+      }
+    }
+    document.addEventListener('keydown', handleEsc)
+
+    return () => {
+      document.removeEventListener('keydown', handleEsc)
+    }
+  }, [])
 
   extend({ OrbitControls })
 
