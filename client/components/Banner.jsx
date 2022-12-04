1248f3b77f08f0036b8fe9d2ef77df2d7d991aad
jay <jay.watt8000@gmail.com>
2022-12-05 12:39:42 +1300

    display/hide text on click

--- client/components/Banner.jsx
+++ client/components/Banner.jsx
@@ -1,27 +1,51 @@
 /* eslint-disable react/no-unknown-property */
-import React from 'react'
+import React, { useRef } from 'react'
 import fonts from './fonts'
+// import { useThree } from '@react-three/fiber'
+// import * as THREE from 'three'
 
 function Banner({ text }) {
+  const ref = useRef()
+
+  // const { camera } = useThree()
+  // let rotation = camera.rotation
+  // const startingVec = new THREE.Vector3();
+  // const startingRotate = camera.getWorldDirection(startingVec)
+  // const startingRotation = [startingRotate.x, startingRotate.y, startingRotate.z]
+
+  // const [rotation, setRotation] = useState(startingRotation)
+  // function getNewRotation() {
+  //   const vec = new THREE.Vector3();
+  //   const rotate = camera.getWorldDirection(vec)
+  //   return [rotate.x, rotate.y, rotate.z]
+  // }
+  // useEffect(() => {
+  //   setRotation(newRotation)
+  //   console.log('setting rotation')
+  // }, [camera.projectionMatrix])
+
+
   const opts = {
     font: 'Philosopher',
-    fontSize: 12,
+    fontSize: 1,
     color: '#99ccff',
-    maxWidth: 300,
+    position: [0, 2, 0.5],
     lineHeight: 1,
     letterSpacing: 0,
     textAlign: 'justify',
+    anchorX: 'center',
+    // rotateX: rotation[0],
+    // rotateY: rotation[1],
+    // rotateZ: rotation[2]
   }
   return (
     <text
+      ref={ref}
       text={text}
       {...opts}
+      localRotation={[100, 0, 0]}
       font={fonts[opts.font]}
-      anchorX="center"
-      anchorY="middle"
-    >
-      <meshPhongMaterial attach="material" color="hotpink" />
-    </text>
+    />
   )
 }
 
--- client/components/BoxTest.jsx
+++ client/components/BoxTest.jsx
@@ -5,21 +5,22 @@ import { extend } from '@react-three/fiber'
 import Sound from './Sound'
 import Banner from './Banner'
 
+
 function BoxTest(props) {
   extend({ Text })
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef()
   // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false)
-  const [clicked, setClicked] = useState(false)
+  const [clicked, click] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
   // useFrame((state, delta) => (ref.current.rotation.x += delta))
   // Return the view, these are regular Threejs elements expressed in JSX
 
-  function handleClick(event) {
-    event.preventDefault()
-    setClicked(!clicked)
-  }
+  // function handleClick(event) {
+  //   event.preventDefault()
+  //   setClicked(!clicked)
+  // }
 
   const audioUrl = `./server/public/audio/${props.data.ML_Catalog_Number}.mp3`
 
@@ -27,15 +28,15 @@ function BoxTest(props) {
     <mesh
       position={props.position}
       ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={handleClick}
+      scale={hovered ? 1 : 1}
+      onClick={() => click(!clicked)}
       onPointerOver={() => hover(true)}
       onPointerOut={() => hover(false)}
     >
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'purple'} />
       <Sound url={audioUrl} />
-      <Banner text={'whatever'} />
+      {clicked && <Banner text={'whatever'} />}
     </mesh>
   )
 }
