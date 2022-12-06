ab3321c1da70fb2eb4971c1756580856aa35940f
Alex Coaton <alex.coaton@devacademy.co.nz>
2022-12-06 15:44:50 +1300

    WIP on fixes.

--- client/components/Bird.jsx
+++ client/components/Bird.jsx
@@ -1,40 +1,51 @@
 /* eslint-disable react/no-unknown-property */
 import React, { useRef, useState } from 'react'
 import { useFrame, useThree } from '@react-three/fiber'
-import { useSpring } from "@react-spring/core"
-import { a } from "@react-spring/three"
+import { useSpring } from '@react-spring/core'
+import { a } from '@react-spring/three'
 
 import Sound from './Sound'
 import Info from './Info'
 
-function Bird({ position, data }) {
+function Bird(props) {
+  const position = props.position
+  const data = props.data
   const bird = useRef(null)
   const { camera } = useThree()
   const audioUrl = `./server/public/audio/${data.Sound_Id}.mp3`
 
-  const [click, setClick] = useState(false)
-  const [camDist, setDist] = useState(0)
-  const [active, setActive] = useState(0)
+  const [birdState, setBirdState] = useState({
+    click: false,
+    camDist: 0,
+    active: 0,
+  })
 
   const minCamDist = 5
   const maxCamDist = 30
   const { spring } = useSpring({
-    spring: active,
-    config: { mass: 5, tension: 400, friction: 50, precision: 0.0001 }
+    spring: birdState.active,
+    config: { mass: 5, tension: 400, friction: 50, precision: 0.0001 },
   })
   const scale = spring.to([0, 1], [1, 5])
   const rotation = spring.to([0, 1], [0, Math.PI])
-  const color = spring.to([0, 1], ["hotpink", "hotpink"])
+  const color = 'hotpink'
 
   useFrame(() => {
     const currentCamPos = camera.position
     const currentBirdPos = bird.current.position
     const currentDist = currentCamPos.distanceTo(currentBirdPos)
-    setDist(currentDist)
-    if ((currentDist > maxCamDist && active) || (currentDist < minCamDist && active)) {
-      setClick(!click)
-      setActive(0)
-      setDist(currentDist)
+    if (
+      (currentDist > maxCamDist && birdState.active) ||
+      (currentDist < minCamDist && birdState.active)
+    ) {
+      setBirdState({
+        ...birdState,
+        click: !birdState.click,
+        active: 0,
+        camDist: currentDist,
+      })
+    } else {
+      setBirdState({ ...birdState, camDist: currentDist })
     }
   })
 
@@ -49,7 +60,6 @@ function Bird({ position, data }) {
     }
   }
 
-
   return (
     <a.mesh
       ref={bird}
@@ -62,7 +72,7 @@ function Bird({ position, data }) {
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={color} />
       <Sound url={audioUrl} />
-      {active && <Info data={data} />}
+      {birdState.active && <Info data={data} />}
     </a.mesh>
   )
 }
