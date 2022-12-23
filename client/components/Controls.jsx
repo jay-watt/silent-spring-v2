5ce1d73364056c0a4bec6ed7a4af35a91f1bad61
Jay Watt <jaywatt8000@gmail.com>
2022-12-23 15:24:30 +1300

    increase speed of controls

--- client/components/Controls.jsx
+++ client/components/Controls.jsx
@@ -16,11 +16,13 @@ function Controls() {
     <FirstPersonControls
       ref={controls}
       activeLook={true}
-      movementSpeed={10}
-      lookSpeed={0.01}
-      constrainVertical={true}
-      verticalMax={Math.PI - 0.25}
-      verticalMin={0.25}
+      movementSpeed={30}
+      lookSpeed={0.03}
+      // movementSpeed={10}
+      // lookSpeed={0.01}
+      // constrainVertical={true}
+      // verticalMax={Math.PI - 0.25}
+      // verticalMin={0.25}
       target={controls.target}
       args={[camera, gl.domElement]}
     />
